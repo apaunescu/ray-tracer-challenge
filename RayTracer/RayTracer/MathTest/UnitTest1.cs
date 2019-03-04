6df81ff1572cb68becf65cd1e4f4f86d00efdbb3
tb <tpa12@sfu.ca>
2019-03-03 23:50:39 -0800

    Update project and create first test.

--- RayTracer/RayTracer/MathTest/UnitTest1.cs
+++ RayTracer/RayTracer/MathTest/UnitTest1.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MathTest
+namespace RayTracerMathTest
 {
     [TestClass]
-    public class UnitTest1
+    public class TupleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ATupleWithWEquals1Dot0IsAPoint()
         {
+            RayTracerMath.Tuple tuple = new RayTracerMath.Tuple(4.3f, -4.2f, 3.1f, 1.0f);
+            Assert.AreEqual(tuple.X, 4.3f);
+            Assert.AreEqual(tuple.Y, -4.2f);
+            Assert.AreEqual(tuple.Z, 3.1f);
+            Assert.AreEqual(tuple.W, 1.0f);
+
+            Assert.AreEqual(tuple.isPoint(), true);
+            Assert.AreEqual(tuple.isVector(), false);
         }
     }
 }
